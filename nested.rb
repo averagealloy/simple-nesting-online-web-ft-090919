4b5aa2ee39abf94f44b747584f2a614b69a6cdf7
Mike McTighe <m.mctighe10@gmail.com>
2019-09-14 19:17:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,3 +129,27 @@ def adding_to_dennis
 programmer_hash[:dennis_richie][:languages] << "Assembly"
 return programmer_hash
 end
+
+
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
+
+end
+
+adding_to_de
