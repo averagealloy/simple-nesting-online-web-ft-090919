fb007921e2756ef64dae183539f47f3929f51a7f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-14 19:15:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_richie][:languages] << "ssembly"
+programmer_hash[:dennis_richie][:languages] << "Assembly"
 end
